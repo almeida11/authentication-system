a9d2ff2e5a6ab25c17492f0763fd9a8bc2d1c71c
Mateus Almeida <mateus.almeidagf@gmail.com>
2023-01-15 20:04:01 -0300

    feat: input email and pass

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import "./styles.css";
+
+//Login Page - Auth
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submite", {email, pass});
+    };
+    return (
+        <div id="login">
+            <h1 className="title">Login</h1>
+            <form className="formLogin" onSubmit={handleSubmit}>
+                <div className="field">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="field">
+                    <label htmlFor="password">Senha</label>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    value={pass} 
+                    onChange={(e) => setPass(e.target.value)}/>
+                </div>
+                <div className="actions">
+                    <button type="submit">Entrar</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage
